11303a736f7705b107b3fa02357b715f944d3585
troublemakerq <55075899+troublemakerq@users.noreply.github.com>
2020-04-19 18:48:36 +0300

    Add files via upload

---
+++ Fraction.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Дроби
+{
+    class Fraction
+    {
+        private double numerator;
+        private double denominator;
+        private double value;
+        public Fraction(double num, double denum)
+        {
+            Numerator = num;
+            Denominator = denum;
+            value = num / denum;
+        }
+        public double Numerator { get => numerator; set => numerator = value; }
+        public double Denominator { get => denominator; set => denominator = value; }
+
+        public void Exponentiation(double num)
+        {
+            Numerator = Math.Pow(Numerator, num);
+            Denominator = Math.Pow(Denominator, num);
+        }
+
+        public static bool operator ==(Fraction fractionA, Fraction fractionB)
+        {
+            return fractionA.value.Equals(fractionB.value);
+        }
+
+        public static bool operator !=(Fraction fractionA, Fraction fractionB)
+        {
+            return !fractionA.value.Equals(fractionB.value);
+        }
+
+        public static bool operator >(Fraction fractionA, Fraction fractionB)
+        {
+            return fractionA.value > fractionB.value;
+        }
+
+        public static bool operator <(Fraction fractionA, Fraction fractionB)
+        {
+            return fractionA.value < fractionB.value;
+        }
+
+        public static bool operator >=(Fraction fractionA, Fraction fractionB)
+        {
+            return fractionA.value >= fractionB.value;
+        }
+
+        public static bool operator <=(Fraction fractionA, Fraction fractionB)
+        {
+            return fractionA.value <= fractionB.value;
+        }
+
+        public static Fraction operator +(Fraction fractionA, Fraction fractionB)
+        {
+            return new Fraction(fractionA.numerator * fractionB.denominator + fractionA.denominator * fractionB.numerator, fractionA.denominator * fractionB.denominator);
+        }
+
+        public static Fraction operator -(Fraction fractionA, Fraction fractionB)
+        {
+            return new Fraction(fractionA.numerator * fractionB.denominator - fractionA.denominator * fractionB.numerator, fractionA.denominator * fractionB.denominator);
+        }
+
+        public static Fraction operator *(Fraction fractionA, Fraction fractionB)
+        {
+            return new Fraction(fractionA.numerator * fractionB.numerator, fractionA.denominator * fractionB.denominator);
+        }
+
+        public static Fraction operator /(Fraction fractionA, Fraction fractionB)
+        {
+            return new Fraction(fractionA.numerator * fractionB.denominator, fractionA.denominator * fractionB.numerator);
+        }
+
+        public static Fraction operator ^(Fraction fractionA, double x)
+        {
+            return new Fraction(Math.Pow(fractionA.numerator, x), Math.Pow(fractionA.denominator, x));
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", numerator, denominator);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Дроби
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = new Fraction(1, 2);
+            Fraction fraction2 = new Fraction(1, 3);
+            Fraction fraction3 = new Fraction(1, 5);
+            var result = (fraction3 - fraction1) * fraction3;
+            Console.WriteLine(result);
+            var result1 = fraction1 + fraction2;
+            Console.WriteLine(result1);
+            var result2 = fraction1 ^ 3;
+            Console.WriteLine(result2);
+            Console.WriteLine(fraction1 > fraction2);
+            Console.WriteLine(fraction1 < fraction2);
+
+        }
+    }
+}
